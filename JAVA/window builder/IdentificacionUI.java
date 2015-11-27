22af736324548e284870a52a4e8fc97c86014275
Igor <igoreslein@gmail.com>
2015-11-27 12:52:33 +0000

    identificacionUI aldaketa

--- JAVA/window builder/IdentificacionUI.java
+++ JAVA/window builder/IdentificacionUI.java
@@ -19,7 +19,8 @@ public class IdentificacionUUI extends JFrame {
 	private JPanel contentPane;
 	private JTextField txtUsuario;
 	private JPasswordField txtPassword;
-
+	private String password="";
+	
 	/**
 	 * Launch the application.
 	 */
@@ -42,7 +43,7 @@ public class IdentificacionUUI extends JFrame {
 	public IdentificacionUUI() {
 		setTitle("IDENTIFICACION");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 329, 168);
+		setBounds(100, 100, 340, 262);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -54,7 +55,7 @@ public class IdentificacionUUI extends JFrame {
 		contentPane.add(lblUsuario);
 		
 		txtUsuario = new JTextField();
-		txtUsuario.setBounds(139, 13, 136, 30);
+		txtUsuario.setBounds(159, 13, 136, 30);
 		contentPane.add(txtUsuario);
 		txtUsuario.setColumns(10);
 		
@@ -64,21 +65,20 @@ public class IdentificacionUUI extends JFrame {
 		contentPane.add(lblPassword);
 		
 		txtPassword = new JPasswordField();
-		txtPassword.setBounds(139, 54, 136, 30);
+		txtPassword.setBounds(159, 54, 136, 30);
 		contentPane.add(txtPassword);
 		
 		JButton btnAceptar = new JButton("aceptar");
 		btnAceptar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String nombre=txtUsuario.getText();
-				String pass=new String(txtPassword.getText());
-				if(nombre.equals("admin") && pass.equals("password")){
+				if(nombre.equals("admin") && password.equals("1234")){
 					HelloworldUUI frmSaludo=new HelloworldUUI();
 					frmSaludo.setVisible(true);
 				}
 			}
 		});
-		btnAceptar.setBounds(52, 93, 89, 23);
+		btnAceptar.setBounds(72, 175, 84, 23);
 		contentPane.add(btnAceptar);
 		
 		JButton btnCancelar = new JButton("cancelar");
@@ -87,7 +87,111 @@ public class IdentificacionUUI extends JFrame {
 				System.exit(0);
 			}
 		});
-		btnCancelar.setBounds(172, 93, 89, 23);
+		btnCancelar.setBounds(178, 175, 84, 23);
 		contentPane.add(btnCancelar);
+		
+		JButton btnNum0 = new JButton("0");
+		btnNum0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				password=password+"0";
+				txtPassword.setText(password);
+			}
+		});
+		btnNum0.setBounds(10, 93, 52, 23);
+		contentPane.add(btnNum0);
+		
+		JButton btnNum1 = new JButton("1");
+		btnNum1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				password=password+"1";
+				txtPassword.setText(password);
+			}
+		});
+		btnNum1.setBounds(72, 93, 52, 23);
+		contentPane.add(btnNum1);
+		
+		JButton btnNum2 = new JButton("2");
+		btnNum2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				password=password+"2";
+				txtPassword.setText(password);
+			}
+		});
+		btnNum2.setBounds(134, 93, 52, 23);
+		contentPane.add(btnNum2);
+		
+		JButton btnNum3 = new JButton("3");
+		btnNum3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				password=password+"3";
+				txtPassword.setText(password);
+			}
+		});
+		btnNum3.setBounds(196, 93, 52, 23);
+		contentPane.add(btnNum3);
+		
+		JButton btnNum4 = new JButton("4");
+		btnNum4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				password=password+"4";
+				txtPassword.setText(password);
+			}
+		});
+		btnNum4.setBounds(258, 93, 52, 23);
+		contentPane.add(btnNum4);
+		
+		JButton btnNum5 = new JButton("5");
+		btnNum5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				password=password+"5";
+				txtPassword.setText(password);
+			}
+		});
+		btnNum5.setBounds(10, 127, 52, 23);
+		contentPane.add(btnNum5);
+		
+		JButton btnNum6 = new JButton("6");
+		btnNum6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				password=password+"6";
+				txtPassword.setText(password);
+			}
+		});
+		btnNum6.setBounds(72, 127, 52, 23);
+		contentPane.add(btnNum6);
+		
+		JButton btnNum7 = new JButton("7");
+		btnNum7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				password=password+"7";
+				txtPassword.setText(password);
+			}
+		});
+		btnNum7.setBounds(134, 127, 52, 23);
+		contentPane.add(btnNum7);
+		
+		JButton btnNum8 = new JButton("8");
+		btnNum8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				password=password+"8";
+				txtPassword.setText(password);
+			}
+		});
+		btnNum8.setBounds(196, 127, 52, 23);
+		contentPane.add(btnNum8);
+		
+		JButton btnNum9 = new JButton("9");
+		btnNum9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				password=password+"9";
+				txtPassword.setText(password);
+			}
+		});
+		btnNum9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNum9.setBounds(258, 127, 52, 23);
+		contentPane.add(btnNum9);
 	}
 }
